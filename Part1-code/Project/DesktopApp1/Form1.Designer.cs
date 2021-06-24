0be6e4cc816e6aab5cb1496e56bb3e52e3b7d58c
zeynep köse <zeynep.j17@gmail.com>
2021-06-24 16:56:08 +0300

    upload

---
+++ Part1-code/Project/DesktopApp1/Form1.Designer.cs
@@ -0,0 +1,184 @@
+namespace DesktopApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.helloWorldLabel = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(623, 126);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ENTER";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // helloWorldLabel
+            // 
+            this.helloWorldLabel.AutoSize = true;
+            this.helloWorldLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.helloWorldLabel.ForeColor = System.Drawing.Color.Red;
+            this.helloWorldLabel.Location = new System.Drawing.Point(346, 9);
+            this.helloWorldLabel.Name = "helloWorldLabel";
+            this.helloWorldLabel.Size = new System.Drawing.Size(307, 38);
+            this.helloWorldLabel.TabIndex = 3;
+            this.helloWorldLabel.Text = "MED ALGORITHM";
+            this.helloWorldLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.helloWorldLabel.Click += new System.EventHandler(this.helloWorldLabel_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(13, 187);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1012, 224);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(176, 132);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(425, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(429, 85);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = " ex: \'antre\'";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(140, 447);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Runtime: ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(293, 447);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 25);
+            this.label4.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(183, 60);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(645, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Enter input as a ‘word’ and lists its first 5 alternative nearest words";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(733, 536);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(286, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = " 2014510056 - Zeynep Köse";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1056, 592);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.helloWorldLabel);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label helloWorldLabel;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Part1-code/Project/DesktopApp1/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace DesktopApp1
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public int medFunction(String str1, String str2, int m, int n)
+        {
+            // Create a table to store results of subproblems 
+            int[,] dp = new int[m + 1, n + 1];
+
+            // Fill d[][] in bottom up manner 
+            for (int i = 0; i <= m; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    // If first string is empty, only option is to  insert all characters of second string 
+                    if (i == 0)
+
+                        dp[i, j] = j;
+
+                    // If second string is empty, only option is to remove all characters of second string 
+                    else if (j == 0)
+                        dp[i, j] = i;
+
+                    // If last characters are same, ignore last char and recur for remaining string 
+                    else if (str1[i - 1] == str2[j - 1])
+                        dp[i, j] = dp[i - 1, j - 1];
+
+                    // If the last character is different, consider all possibilities and find the minimum 
+                    else
+                        dp[i, j] = 1 + minValue(dp[i, j - 1], // Insert 
+                                       dp[i - 1, j], // Remove 
+                                       dp[i - 1, j - 1]); // Replace 
+                }
+            }
+
+            return dp[m, n];
+        }
+
+        public int minValue(int x, int y, int z)
+        {
+            if (x <= y && x <= z) return x;
+            if (y <= x && y <= z) return y;
+            else return z;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            List<string> wordList = new List<string>();
+            foreach (string line in File.ReadLines("sozluk.txt", Encoding.GetEncoding("Windows-1254")))
+            {
+                wordList.Add(line);
+            }
+
+            string myString = textBox1.Text;
+            string[] myWords = myString.ToLower().Split(' ');
+            int[] indexArray = new int[myWords.Length];
+
+
+            Queue<string> alternatives = new Queue<string>();
+            List<string> resultList = new List<string>();
+            List<string> resultsList = new List<string>();
+
+            for (int i = 0; i < myWords.Length; i++)
+            {
+                if (wordList.Contains(myWords[i]))
+                {
+                    continue;
+                }
+                int minimumValue = 100;
+                for (int j = 0; j < wordList.Count; j++)
+                {
+                    int temp = (medFunction(myWords[i], wordList[j], myWords[i].Length, wordList[j].ToString().Length));
+
+                    if (minimumValue >= temp)
+                    {
+                        minimumValue = temp;
+                        indexArray[i] = j;
+
+                        if (alternatives.Count >= 5)
+                        {
+                            alternatives.Dequeue();
+                            alternatives.Enqueue(wordList[j]);
+                        }
+                        else
+                        {
+                            alternatives.Enqueue(wordList[j]);
+                        }
+                    }
+                }
+
+                string result = myWords[i] + " -------->> ";
+
+
+                alternatives = new Queue<string>(alternatives.Reverse());
+
+
+                while (alternatives.Count > 0)
+                {
+                    listBox1.Items.Add(alternatives.Dequeue());
+
+
+                }
+
+            }
+
+            stopWatch.Stop();
+
+            label4.Text = stopWatch.ElapsedMilliseconds.ToString() + " ms";
+
+        }
+
+        private void helloWorldLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
